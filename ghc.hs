c7e80b2a86fdf21811159679ab495125245b28ca
Kazuki Okamoto <kazukiokamoto@iij.ad.jp>
2019-07-29 16:09:18 +0900

    package-db

--- ghc.hs
+++ ghc.hs
@@ -1,11 +1,12 @@
 module Main where
 
 import System.Environment
-import System.FilePath
 import System.Process
 
 main :: IO ()
 main = do
   args <- getArgs
-  compilerBin <- head . lines <$> readProcess "stack" ["path", "--compiler-bin"] ""
-  callProcess (compilerBin </> "ghc") args
+  compilerExe <- head . lines <$> readProcess "stack" ["path", "--compiler-exe"] ""
+  snapshotPackageDB <- head .lines <$> readProcess "stack" ["path", "--snapshot-pkg-db"] ""
+  localPackageDB <- head .lines <$> readProcess "stack" ["path", "--local-pkg-db"] ""
+  callProcess compilerExe ("-package-db":snapshotPackageDB:"-package-db":localPackageDB:args)
