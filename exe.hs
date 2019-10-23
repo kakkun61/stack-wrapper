18cbb29aa48591b0850f09a3eadcf1f468bdaa4a
Kazuki Okamoto <kazukiokamoto@iij.ad.jp>
2019-10-23 17:47:18 +0900

    for arbitrary executables

---
+++ exe.hs
@@ -0,0 +1,10 @@
+module Main where
+
+import System.Process
+import System.Environment
+
+main :: IO ()
+main = do
+  name <- getProgName
+  args <- getArgs
+  callProcess "stack" ("exec":"--":name:args)
